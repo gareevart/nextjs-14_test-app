926bae671fcce6330ad2361180bfb1e248bad03f
Dmitry Gareev <gareev2692@ya.ru>
2024-03-13 23:08:43 +0100

    Added WebAnalytics module

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,8 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import Image from 'next/image';
 import styles from "./home.module.css";
 import Link from 'next/link';
-import Button from '@/components/ui/button/button';
 
 const Home = () => {
   return <div className={styles.container}>
@@ -21,6 +21,7 @@ const Home = () => {
       <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
     </div>
     <SpeedInsights />
+    <Analytics />
   </div>
 };
 
